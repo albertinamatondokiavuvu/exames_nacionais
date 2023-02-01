e55be7abd16c07d1888c48bfd031eb4a9cafcbf1
pedro3mdev <manuel3m1001@gmail.com>
2023-02-01 12:29:33 +0100

    iuytre

---
+++ resources/views/forms/_FormCentroExame/index.blade.php
@@ -0,0 +1,5 @@
+<div class="mb-3">
+    <input type="text" name="nome_centro"
+        class="form-control" id="setting-input-1" placeholder="Nome do Centro" required>
+</div>
+
--- resources/views/forms/_FormTurma/index.blade.php
+++ resources/views/forms/_FormTurma/index.blade.php
@@ -1,26 +1,5 @@
 <div class="mb-3">
-    <input type="text" name="nome_turma"  value="{{ isset($turmas->nome_turma) ? $turmas->nome_turma : '' }}"
-        class="form-control" id="setting-input-1" placeholder="Nome da Turma" required>
+    <input type="text" name="name"  value="{{ isset($users->name) ? $users->name : '' }}"
+        class="form-control" id="setting-input-1" placeholder="Nome" required>
 </div>
-<div class="row g-3 mb-3">
-    <div class="col-sm">
-        <input type="number" name="quantidade"
-            value="{{ isset($turmas->quantidade) ? $turmas->quantidade : '' }}" class="form-control" id="setting-input-1"
-            placeholder="quantidade de alunos nesta turma" required>
-    </div>
-    <div class="col-sm">
-       <select name="classe_id" id="" class="form-control">
-        @isset($turmas->classe_id)
-        <option hidden style="color: green" selected value="{{ $turmas->classe_id }}"></option>
-        @else
-            <option selected disabled>Selecione a classe</option>
-        @endisset
-        @foreach ($classes as $classes )
-        <option value="{{ $classes->id }}" >{{ $classes->nome_classe }}</option>
-        @endforeach
-
-       </select>
-    </div>
-</div>
-
 
