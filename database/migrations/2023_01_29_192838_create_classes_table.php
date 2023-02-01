8460454efd5760eb3f694d3a3e45b290c86c0b3d
albertinamatondokiavuvu <albertinakiavuvu11@gmail.com>
2023-02-01 12:58:17 +0100

    d

--- database/migrations/2023_01_29_192838_create_classes_table.php
+++ database/migrations/2023_01_29_192838_create_classes_table.php
@@ -16,9 +16,8 @@ class CreateClassesTable extends Migration
         Schema::create('classes', function (Blueprint $table) {
             $table->id();
             $table->string('nome_classe');
-            $table->foreignId('user_id')
-            ->constrained()
-            ->onDelete('cascade');
+            $table->string('provincia');
+            $table->string('municipio');
             $table->timestamps();
         });
     }
